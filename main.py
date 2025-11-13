3366a4bf3cd13ac9a77d35be4a427e1897d751b3
Daniel <d0583258844@gmail.com>
2025-11-13 12:26:29 +0200

    I added a func of encrypt/decrypt

--- main.py
+++ main.py
@@ -15,5 +15,28 @@ def get_name_and_save(name: str):
     return {"msg": "saved user"}
 
 
+@app.post("/caesar")
+def caesar_encrypted_decrypted(item:dict):
+    text = item["text"]
+    text1 = text.replace(" ","")
+    alph_list =[
+                'a', 'b', 'c', 'd', 'e',
+                'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't', 
+                'u', 'v', 'w', 'x', 'y', 'z'
+            ]
+    new_word = ""
+    if item["mode"] == "encrypt":
+        for ch in text1:
+            ch1 = alph_list.index(ch)
+            new_word += alph_list[(ch1 + item["offset"]) % 26] 
+        return new_word
+
+    elif item["mode"] == "decrypt":
+        for ch in text1:
+            ch1 = alph_list.index(ch)
+            new_word += alph_list[(ch1 - item["offset"]) % 26] 
+    return new_word
 
 
